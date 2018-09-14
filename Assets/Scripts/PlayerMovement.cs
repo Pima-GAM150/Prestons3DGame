574b07e6e486584c738eea66017599b0f07868e2
Psheldon <psheldon@mail.pima.edu>
2018-09-13 20:50:38 -0700

    Imported local repo to cloud.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Transform transform;
+    public float jumpPower;
+    public float speed;
+    public float maxSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GoForward();    
+	}
+
+    void GoForward ()
+    {
+        transform.position = new Vector3((transform.position.x + (speed * Time.deltaTime)), transform.position.y);
+
+        if (speed <= maxSpeed){
+            speed += Time.deltaTime; 
+           }
+    }
+
+}
