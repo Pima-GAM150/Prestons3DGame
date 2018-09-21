6ee96bdce48ec1c05b2f0dd909e7b58ac2d96f6f
Psheldon <psheldon@mail.pima.edu>
2018-09-20 18:52:04 -0700

    Added jumping to playermovement script.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,20 +5,29 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
     public new Transform transform;
+    public Rigidbody rb;
+    public SphereCollider sc;
     public float jumpPower;
     public float speed;
     public float horizSpeed;
     public float maxSpeed;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    private Vector3 jump = new Vector3(0f, 2f, 0f);
+    private bool isGrounded;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update () {
         GoForward();
-        Debug.Log("Horizontal is at " + Input.GetAxis("Horizontal"));
+        Debug.Log("Jump is " + Input.GetButtonDown("Jump"));
+        if (Input.GetButtonDown("Jump") == true && isGrounded)
+        {
+            Jump(rb, jump, jumpPower);
+        }
         if(Input.GetAxis("Horizontal") != 0)
         {
             transform.position = new Vector3((transform.position.x + (Input.GetAxis("Horizontal") * horizSpeed * Time.deltaTime)), transform.position.y, transform.position.z);
@@ -36,14 +45,33 @@ public class PlayerMovement : MonoBehaviour {
            }
     }
 
-    void MoveHorizontal()
+    void Jump(Rigidbody rb, Vector3 direction, float x)
     {
-        
+        rb.AddForce(direction * jumpPower, ForceMode.Impulse);
     }
 
-    void Jump(float x)
+    //This function updates the isGrounded variable when the player lands after a jump.
+    void OnCollisionEnter(Collision collision)
     {
+        if(collision.gameObject.tag == "ground")
+        {
+            isGrounded = true;
+        }
 
+        if(collision.gameObject.tag == "obstacle")
+        {
+            Crashed();
+        }
+    }
+
+    //This function updates the isGrounded variable to false when the player jumps, 
+    //not allowing for another jump.
+    private void OnCollisionExit(Collision collision)
+    {
+        if(collision.gameObject.tag == "ground")
+        {
+            isGrounded = false;
+        }
     }
 
     void Crashed()
